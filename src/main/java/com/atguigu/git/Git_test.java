87eab413bf69d4360dbdbe22188f7147ff2d4db4
Jason <Jason@qq.com>
2022-11-24 17:03:16 +0800

    push commit

--- src/main/java/com/atguigu/git/Git_test.java
+++ src/main/java/com/atguigu/git/Git_test.java
@@ -16,5 +16,6 @@ public class Git_test {
         System.out.println("hello git4");
         System.out.println("master test");
         System.out.println("hot-fix test");
+        System.out.println("Push test");
     }
 }
