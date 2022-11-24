814eedd7d2c86e036734bce0512987df6297095e
Jason <Jason@qq.com>
2022-11-24 14:18:32 +0800

    first commit

---
+++ src/main/java/com/atguigu/git/Git_test.java
@@ -0,0 +1,15 @@
+package com.atguigu.git;
+
+/**
+ * Created with IntelliJ IDEA.
+ * User: ÏşÏşÏşÏş¶«
+ * Date: 2022/11/24
+ * Time: 14:04
+ * To change this template use File | Settings | File Templates.
+ * Description:
+ */
+public class Git_test {
+    public static void main(String[] args) {
+        System.out.println("hello git");
+    }
+}
